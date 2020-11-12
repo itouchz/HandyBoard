c6ca90f48b8beb4afdcafb72d886ffad9dde7fe6
Peerapon <bunverdenz@gmail.com>
2020-11-13 02:59:58 +0700

    add phrases

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const BLOCK={
+    1:["video camera with a zoom lens", "nuke the fridge", 
+    "wetter than an otters pocket", "a story as old as time", "the bewildered tourist"],
+    2:["Rush B Cyka Blyat","Taking my dog for a walk", "To see Niagara Falls",
+    "Going for ice cream", "The lost puppy"],
+    3:["a wet and stinky dog", "a cool and wet afternoon", "Strolling along a beach at sunset",
+    "Singing FoR hiS supper", "the long and winding road"],
+    4:["Reading novels", "friendly nature", "very interesting", "A loT Of", "in A hurry"],
+    5:["By working aimlessly", "provided that you come", "What a pity",
+     "I won the first prize", "In the end"]
+};
